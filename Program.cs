df14d3758bdd34da4bef8008a1d2456128a02299
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-11-11 15:32:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dongular_while_foreach
+{
+    class program
+    {
+        private static int sayi;
+
+        static void Main(string[]args)
+        { 
+          
+            Console.Write("***** foreach*****");
+            string[]arabalar ={"bmw","audi","mercedes","volvo"};
+
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+
+
+            }
+             
+                
+            
+
+    
+        }
+    }   
+}       
+
